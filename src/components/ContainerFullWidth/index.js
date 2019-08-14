63bcc7158cd50a067ae22e4a73f2d435270e8841
Cristiano <cristiano.goncalves@cubos.io>
2019-08-14 00:05:03 -0300

    feature: create componente ContainerFullWidth.

---
+++ src/components/ContainerFullWidth/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+import * as S from "./styled";
+
+export default function ContainerFullWidth({ children }) {
+	return <S.ContainerFullWidth>{children}</S.ContainerFullWidth>;
+}
---
+++ src/components/ContainerFullWidth/styled.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const ContainerFullWidth = styled.main`
+	width: 100vw;
+	height: 100vh;
+	overflow: hidden;
+	margin: 0;
+	padding: 0;
+	background: #48395b;
+	color: #fff;
+`;
