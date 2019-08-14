278b9dc0e4ab87b7438a7fe1ac6fcd2623f52dc9
Cristiano <cristiano.goncalves@cubos.io>
2019-08-14 00:08:19 -0300

    feature: create PresentationText component.

---
+++ src/components/PresentationText/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import * as S from "./styled";
+
+export default function PresentationText() {
+	return (
+		<S.PresentationContainer>
+			<S.PresentationText>
+				<span>HI, I'M</span> CRISTIANO GONÇALVES
+			</S.PresentationText>
+
+			<S.JobRole>FrontEnd Developer</S.JobRole>
+		</S.PresentationContainer>
+	);
+}
---
+++ src/components/PresentationText/styled.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const PresentationContainer = styled.div`
+	width: 400px;
+	line-height: 3.5rem;
+	font-weight: bolder;
+`;
+
+export const PresentationText = styled.h1`
+	color: #271f38;
+	font-size: 3rem;
+
+	span {
+		color: #fff;
+	}
+`;
+
+export const JobRole = styled.h2`
+	color: #3fcbc6;
+	font-size: 1.5rem;
+`;
